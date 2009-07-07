d8776296b55e5593db534d0a773f2edb54727689
vilela.lvt <vilela.lvt@cf31a116-6a39-11de-a9b1-77b8bb241e6b>
2009-07-07 15:51:14 +0000

    Registradores e Banco de Registradores estão prontos.

---
+++ trunk/src/memoria.h
@@ -0,0 +1,55 @@
+#ifndef __MEMORIA_H_INCLUDED__
+#define __MEMORIA_H_INCLUDED__
+
+/*
+ *
+ *      Arquivo: memoria.h
+ *        Autor: Leonardo Vilela Teixeira
+ *        email: vilela@dcc.ufmg.br
+ *
+ */
+
+#include "defs.h"
+
+#include <stdbool.h>
+
+/* ************************************************************************** */
+/* ========================== ESTRUTURAS DE DADOS =========================== */
+/* ************************************************************************** */
+
+typedef bool Registrador[BITS_ARQ];
+
+/* ========================================================================== */
+
+typedef struct bnc_reg_str
+{
+    Registrador Regs[NUM_REGS];
+} Banco_de_Registradores;
+
+/* ========================================================================== */
+
+typedef struct mem_str
+{
+    Word posicao[TAM_MEMORIA];
+}
+
+/* ************************************************************************** */
+/* ================================ FUNÇÕES ================================= */
+/* ************************************************************************** */
+
+void Reg_Escreve_Word( Registrador R, Word W );
+void Reg_Le_Word( Registrador R, Word Destino );
+
+/* ========================================================================== */
+
+void B_Reg_Escreve_Word( Banco_de_Registradores B, bool reg_num[3], Word D );
+void B_Reg_Le_Word( Banco_de_Registradores B, bool reg_num[3], Word Destino );
+
+/* ========================================================================== */
+
+void Mem_Le_Endereco( Memoria M )
+void Mem_Escreve_Endereco( Memoria M );
+
+/* ************************************************************************** */
+
+#endif /* __MEMORIA_H_INCLUDED__ */

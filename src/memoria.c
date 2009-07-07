fd509cbb8edc13dcfec3b539ce8a25760d55abb7
vilela.lvt <vilela.lvt@cf31a116-6a39-11de-a9b1-77b8bb241e6b>
2009-07-07 18:52:42 +0000

    Módulo memória pronto. Banco de Registradores e Memória estão implementados.

--- src/memoria.c
+++ src/memoria.c
@@ -12,11 +12,11 @@
 /* ============================== REGISTRADOR =============================== */
 /* ************************************************************************** */
 
-void Reg_Escreve_Word( Registrador R, Word W )
+void Reg_Escreve_Word( Registrador * R, Word W )
 {
     int i;
     for (i = 0 ; i < BITS_ARQ ; i++)
-        R[i] = W[i];
+        (*R)[i] = W[i];
 }
 
 /* ========================================================================== */
@@ -33,7 +33,7 @@ void Reg_Le_Word( Registrador R, Word Destino )
 /* ========================= BANCO DE REGISTRADORES ========================= */
 /* ************************************************************************** */
 
-void B_Reg_Escreve_Word( Banco_de_Registradores B, bool reg_num[3], Word D )
+void B_Reg_Escreve_Word( Banco_de_Registradores * B, bool reg_num[3], Word D )
 {
     /* Calcula Endereço */
     int End = 0;
@@ -44,7 +44,7 @@ void B_Reg_Escreve_Word( Banco_de_Registradores B, bool reg_num[3], Word D )
     /* Copia Word */
     int i;
     for (i = 0 ; i < BITS_ARQ ; i++)
-        B.Regs[End][i] = D[i];
+        B->Regs[End][i] = D[i];
 }
 
 /* ========================================================================== */
@@ -60,8 +60,50 @@ void B_Reg_Le_Word( Banco_de_Registradores B, bool reg_num[3], Word Destino )
     /* Copia Word */
     int i;
     for (i = 0 ; i < BITS_ARQ ; i++)
-        Destino[i] = B.Regs[End][i];
+    {
+       Destino[i] = B.Regs[End][i];
+    }
 
+}
+
+/* ************************************************************************** */
+/* ================================ MEMÓRIA ================================= */
+/* ************************************************************************** */
+
+void Mem_Escreve_Endereco( Memoria * M, Word Endereco, Word Dados )
+{
+    /* Cálculo do Endereço */
+    int i;
+    unsigned int End = 0;
+    for (i = 0 ; i < BITS_ARQ ; i++)
+    {
+        End += Endereco[i] << (BITS_ARQ-i-1);
+    }
+
+    /* Copia Word */
+    for ( i = 0 ; i < BITS_ARQ ; i++)
+    {
+        M->posicao[End][i] = Dados[i];
+    }
+}
+
+/* ========================================================================== */
+
+void Mem_Le_Endereco( Memoria M, Word Endereco, Word Destino )
+{
+    /* Cálculo do Endereço */
+    int i;
+    unsigned int End = 0;
+    for (i = 0 ; i < BITS_ARQ ; i++)
+    {
+        End += Endereco[i] << (BITS_ARQ-i-1);
+    }
+
+    /* Copia a Word */
+    for (i = 0 ; i< BITS_ARQ ; i++)
+    {
+        Destino[i] = M.posicao[End][i];
+    }
 }
 
 /* ************************************************************************** */
--- src/memoria.h
+++ src/memoria.h
@@ -31,24 +31,24 @@ typedef struct bnc_reg_str
 typedef struct mem_str
 {
     Word posicao[TAM_MEMORIA];
-}
+} Memoria;
 
 /* ************************************************************************** */
 /* ================================ FUNÇÕES ================================= */
 /* ************************************************************************** */
 
-void Reg_Escreve_Word( Registrador R, Word W );
+void Reg_Escreve_Word( Registrador * R, Word W );
 void Reg_Le_Word( Registrador R, Word Destino );
 
 /* ========================================================================== */
 
-void B_Reg_Escreve_Word( Banco_de_Registradores B, bool reg_num[3], Word D );
+void B_Reg_Escreve_Word( Banco_de_Registradores * B, bool reg_num[3], Word D );
 void B_Reg_Le_Word( Banco_de_Registradores B, bool reg_num[3], Word Destino );
 
 /* ========================================================================== */
 
-void Mem_Le_Endereco( Memoria M )
-void Mem_Escreve_Endereco( Memoria M );
+void Mem_Escreve_Endereco( Memoria * M, Word Endereco, Word Dados );
+void Mem_Le_Endereco( Memoria M, Word Endereco, Word Destino );
 
 /* ************************************************************************** */
 

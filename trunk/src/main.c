388779e817dc690b58ea837479b8bbf21fd5f6b1
lucasagfranca <lucasagfranca@cf31a116-6a39-11de-a9b1-77b8bb241e6b>
2009-07-09 07:15:08 +0000

    TUDO PRONTOOOOOOOO

--- trunk/src/main.c
+++ trunk/src/main.c
@@ -2,11 +2,21 @@
  * =============================================================================
  * 
  *      Arquivo: main.c
- *        Autor: 
- *        email:
+ *        Autores: Letícia Lana Cherchiglia, Leonardo Vilela Teixeira, Lucas Alves Guerra França
+ *        email: letslc@dcc.ufmg.br, vilela@dcc.ufmg.br, lguerra@dcc.ufmg.br
  *
  * =============================================================================
  */
+
+#include "defs.h"
+#include "registradores.h"
+#include "memoria.h"
+#include "alu.h"
+#include "constantes.h"
+#include "jumps.h"
+#include "controle.h"
+#include "es.h"
+#include "processador.h"
 
 #include <stdlib.h>
 #include <stdio.h>
@@ -15,8 +25,32 @@
 
 int main(int argc, char * argv[])
 {
-    printf("Hello World!\n");
-    return EXIT_SUCCESS;
+
+    //Declara variáveis e as inicializa por meio da função seguinte.
+    char *nome_arq_entrada = NULL;
+    char *dump_pos_inicial = NULL;
+    int dump_num_palavras = 0;
+    bool screen = false;
+    bool pausa = false;
+
+    es_Inicializa_Variaveis_Parametros_Execucao( int *argc, char ***argv, &nome_arq_entrada, &dump_pos_inicial, &dump_num_palavras, &screen, &pausa );
+
+    //Cria e inicializa um processador.
+    Processador processador;
+    Inicializa_Processador(&processador);
+
+    //Abre o arquivo de entrada.
+    FILE *arq_entrada;
+    abreArquivoLeitura(&arq_entrada, nome_arq_entrada);
+
+    //Carrega a memória a partir dos dados do arquivo de entrada.
+    es_Le_Arquivo_Armazena_Instrucoes_Memoria(&arq_entrada, &processador->memoria);
+
+    //Inicia o processamento.
+    Executa_Processamento(&processador, &screen, &dump_pos_inicial, &dump_num_palavras);
+
+    //Fecha o arquivo de entrada.
+    fclose(arq_entrada);    
 }
 
 /* ========================================================================== */
